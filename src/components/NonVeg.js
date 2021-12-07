8b98c389df87ef4d53e03583902011b203d71e65
Deepak0844 <91111526+Deepak0844@users.noreply.github.com>
2021-12-07 18:04:29 +0530

    Update NonVeg.js

--- src/components/NonVeg.js
+++ src/components/NonVeg.js
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 export function NonVeg() {
   const [pizza, setPizza] = useState([]);
   const getPizza = () => {
-    fetch("http://localhost:9000/pizza")
+    fetch("https://testing-api-hackathon.herokuapp.com/pizza")
       .then((data) => data.json())
       .then((pza) => setPizza(pza));
   };
@@ -41,7 +41,7 @@ function NonVegList({ name, description, image, price, pizza, index }) {
     const result = pizza[index];
     const same = cart.map(({ name }) => name === result.name);
     console.log("same:", same);
-    fetch("http://localhost:9000/cart")
+    fetch("https://testing-api-hackathon.herokuapp.com/cart")
       .then((data) => data.json())
       .then((crt) => setCart(crt));
 
@@ -53,7 +53,7 @@ function NonVegList({ name, description, image, price, pizza, index }) {
         quantity: "",
       };
 
-      fetch(`http://localhost:9000/cart`, {
+      fetch(`https://testing-api-hackathon.herokuapp.com/cart`, {
         method: "POST",
         body: JSON.stringify(cartItem),
         headers: {
